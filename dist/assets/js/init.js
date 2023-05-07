2b7f38acaa6a2b0fb1b62631946590e39b361130
shoji <sk_69@outlook.jp>
2023-05-07 13:27:02 +0900

    調整内容

--- dist/assets/js/init.js
+++ dist/assets/js/init.js
@@ -1,31 +1,20 @@
 
-/**
- * フェードイン
- */
-gsap.utils.toArray('.js-fadein').forEach(target => {
-    gsap.fromTo(target, {
-        opacity: 0,
-        ease: "power1.inOut",
-        }, {
-            opacity: 1,
-            scrollTrigger: {
-                trigger: target,
-                scrub: 1,
-                start: "top bottom",
-                end: "center center"
-            },
-        }
-    );
-});
 
 $(function(){
 
 	/** 
-	 * 共通ページ 
 	 * Swiperを使ったjsコード
 	 * https://swiperjs.com/
 	 */
 	slideAll();
+	/** 
+	 * faqアコーディオンメニュー 
+	 */
+	acd();
+	/** 
+	 * フェードイン
+	 */
+	fadein();
 	
 });
 
@@ -37,11 +26,81 @@ var slideAll = function(){
 		spaceBetween: 90,
 		slidesPerView: 2,
 		centeredSlides: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true, 
+		  },
 	});
 	var swiper_voice = new Swiper(".js-voiceSlide", {
 		loop: false,
 		spaceBetween: 115,
 		slidesPerView: 1.4,
 		centeredSlides: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true, 
+		  },
+	});
+}
+
+var acd = function(){
+	$('.js-acd').click(function() {
+		$(this).toggleClass('-active');
+		$(this).next().slideToggle(400);
+	});
+}
+
+
+var fadein = function() {
+	gsap.utils.toArray('.js-fadein').forEach(target => {
+		gsap.fromTo(target, {
+			opacity: 0,
+			yPercent: 5,
+			ease: "power1.inOut",
+        }, {
+			opacity: 1,
+			yPercent: 0,
+            scrollTrigger: {
+				trigger: target,
+                scrub: 1,
+                start: "top bottom",
+                end: "center center+=50%"
+            },
+        }
+		);
+	});
+	gsap.utils.toArray('.js-slideinleft').forEach(target => {
+		gsap.fromTo(target, {
+			x: -100,
+			opacity: 0,
+			ease: "power1.inOut",
+        }, {
+			x: 0,
+			opacity: 1,
+            scrollTrigger: {
+				trigger: target,
+                scrub: 1,
+                start: "top bottom",
+                end: "center center+=20%"
+            },
+        }
+		);
+	});
+	gsap.utils.toArray('.js-slideinright').forEach(target => {
+		gsap.fromTo(target, {
+			x: 100,
+			opacity: 0,
+			ease: "power1.inOut",
+        }, {
+			x: 0,
+			opacity: 1,
+            scrollTrigger: {
+				trigger: target,
+                scrub: 1,
+                start: "top bottom",
+                end: "center center+=20%"
+            },
+        }
+		);
 	});
 }
